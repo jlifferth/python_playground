f14cebf7126b24dc4b4039028dc96d4b6af9db59
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2022-02-08 19:49:01 -0600

    showing basic python to cousin Carson

---
+++ for_carson/hello_world.py
@@ -0,0 +1,8 @@
+name = "Carson"
+age = 15
+weight = 125.5
+full = True
+
+# print("Hello World")
+# print("Hello ", name)
+print("Your age is {}, your weight is {}. Your weight + your age = {}".format(age, weight, (age + weight)))
